cab99f05d1276969790f77c1d7db85d7fb951cec
Akash Badshah <akashbad@mit.edu>
2014-05-02 13:16:11 -0400

    visualization for single sensor board

--- Final Project/scratchpad.asm
+++ Final Project/scratchpad.asm
@@ -3,9 +3,100 @@
 ; small circuits and quick code
 
 .org 00h
-start:
-  loop: 
-    djnz r0, loop
-    djnz r1, loop
-  cpl p1.0
-  sjmp loop
+;=================================================================
+; subroutine init
+; this routine initializes the hardware
+; set up serial port with a 11.0592 MHz crystal,
+; use timer 1 for 9600 baud serial communications
+;=================================================================
+init:
+   mov   tmod, #20h       ; set timer 1 for auto reload - mode 2
+   mov   tcon, #41h       ; run counter 1 and set edge trig ints
+   mov   th1,  #0fdh      ; set 9600 baud with xtal=11.059mhz
+   mov   scon, #50h       ; set serial control reg for 8 bit data
+                          ; and mode 1
+   mov r1, #00h 
+loop:
+  mov p1, r1
+  inc r1
+  mov dptr, #0fe00h
+  movx @dptr, a
+  movx a, @dptr  
+  lcall prthex
+  mov a, #20h
+  lcall sndchr
+  cjne r1, #32, loop
+  mov r1, #00h 
+  lcall crlf
+  ;lcall getchr
+  sjmp loop   
+;===============================================================
+; subroutine sndchr
+; this routine takes the chr in the acc and sends it out the
+; serial port.
+;===============================================================
+sndchr:
+   clr  scon.1            ; clear the tx  buffer full flag.
+   mov  sbuf,a            ; put chr in sbuf
+txloop:
+   jnb  scon.1, txloop    ; wait till chr is sent
+   ret
+;===============================================================
+; subroutine getchr
+; this routine reads in a chr from the serial port and saves it
+; in the accumulator.
+;===============================================================
+getchr:
+   jnb  ri, getchr        ; wait till character received
+   mov  a,  sbuf          ; get character
+   anl  a,  #7fh          ; mask off 8th bit
+   clr  ri                ; clear serial status bit
+   ret
+
+;===============================================================
+; subroutine prthex
+; this routine takes the contents of the acc and prints it out
+; as a 2 digit ascii hex number.
+;===============================================================
+prthex:
+   push acc
+   lcall binasc           ; convert acc to ascii
+   lcall sndchr           ; print first ascii hex digit
+   mov   a,  r2           ; get second ascii hex digit
+   lcall sndchr           ; print it
+   pop acc
+   ret
+;===============================================================
+; subroutine binasc
+; binasc takes the contents of the accumulator and converts it
+; into two ascii hex numbers.  the result is returned in the
+; accumulator and r2.
+;===============================================================
+binasc:
+   mov   r2, a            ; save in r2
+   anl   a,  #0fh         ; convert least sig digit.
+   add   a,  #0f6h        ; adjust it
+   jnc   noadj1           ; if a-f then readjust
+   add   a,  #07h
+noadj1:
+   add   a,  #3ah         ; make ascii
+   xch   a,  r2           ; put result in reg 2
+   swap  a                ; convert most sig digit
+   anl   a,  #0fh         ; look at least sig half of acc
+   add   a,  #0f6h        ; adjust it
+   jnc   noadj2           ; if a-f then re-adjust
+   add   a,  #07h
+noadj2:
+   add   a,  #3ah         ; make ascii
+   ret
+;===============================================================
+; subroutine crlf
+; crlf sends a carriage return line feed out the serial port
+;===============================================================
+crlf:
+   mov   a,  #0ah         ; print lf
+   lcall sndchr
+cret:
+   mov   a,  #0dh         ; print cr
+   lcall sndchr
+   ret
---
+++ Final Project/visualization.py
@@ -0,0 +1,22 @@
+import sys, serial
+from matplotlib import pyplot
+
+ser = serial.Serial("/dev/tty.usbserial",9600)
+pyplot.ion()
+pyplot.ylim([0,50])
+plot, = pyplot.plot([0 for x in range(32)])
+while True:
+  try:
+    #ser.write(' ')
+    line = ser.readline()
+    data = [int(x,16) for x in line.split()]
+    plot.set_ydata(data)
+    pyplot.draw()
+  except KeyboardInterrupt:
+    print 'exiting'
+    break
+  except ValueError:
+    print 'deformed data'
+    continue
+ser.flush()
+ser.close()
